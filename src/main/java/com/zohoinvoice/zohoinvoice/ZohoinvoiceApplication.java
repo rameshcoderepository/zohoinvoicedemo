92bcf41b4e89a970f052f3760f094a5c923d7661
ramesh <rameshkumar6plus@gmail.com>
2023-08-01 12:16:38 +0530

    dummy commit

---
+++ src/main/java/com/zohoinvoice/zohoinvoice/ZohoinvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.zohoinvoice.zohoinvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZohoinvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZohoinvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zohoinvoice/zohoinvoice/ZohoinvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zohoinvoice.zohoinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZohoinvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
